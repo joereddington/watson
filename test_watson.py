1d9ffb33bb911d8490aeddc3fa2ed8c7a1a289c7
Joe Reddington <joe@joereddington.com>
2018-01-01 15:06:42 +0000

    Significant refactoring, all tests passing

--- test_watson.py
+++ test_watson.py
@@ -4,6 +4,8 @@ import watson
 import urllib
 import json
 import os
+import atom
+import session
 import datetime
 from urllib2 import urlopen, Request
 
@@ -14,7 +16,7 @@ class watsonTest(TestCase):
     def test_fast_strptime(self):
         test1="02/07/17 15:22"
         TIME_FORMAT = "%d/%m/%y %H:%M"
-        result=watson.fastStrptime(test1,TIME_FORMAT)
+        result=atom.fastStrptime(test1,TIME_FORMAT)
         otherresult=datetime.datetime.strptime(test1,TIME_FORMAT)
 
         self.assertEqual(result,otherresult)
@@ -23,7 +25,7 @@ class watsonTest(TestCase):
     def test_fast_strptime_from_watch(self):
         test1="01-Jan-2018 07:22"
         TIME_FORMAT = "%d-%b-%Y %H:%M"
-        result=watson.fastStrptime(test1,TIME_FORMAT)
+        result=atom.fastStrptime(test1,TIME_FORMAT)
         otherresult=datetime.datetime.strptime(test1,TIME_FORMAT)
 
         self.assertEqual(result,otherresult)
@@ -33,47 +35,47 @@ class watsonTest(TestCase):
     def test_download_repo_json(self):
          self.assertEqual(3,3)
 
-    def test_read_log_file(self):
-        atoms=watson.read_log_file("testinputs/regressions/livenotes.md")
+    def test_log_file_to_atoms(self):
+        atoms=watson.log_file_to_atoms("testinputs/regressions/livenotes.md")
         self.assertEqual(len(atoms),582)
 
-    def test_read_log_file_inline(self):
-        atoms=watson.read_log_file("testinputs/regressions/livenotesinline.md")
+    def test_log_file_to_atoms_inline(self):
+        atoms=watson.log_file_to_atoms("testinputs/regressions/livenotesinline.md")
         sessions=watson.get_sessions(atoms)
         self.assertEqual(len(atoms),582)
 
-    def test_read_log_file_problem(self):
-        atoms=watson.read_log_file("testinputs/problem.md")
+    def test_log_file_to_atoms_problem(self):
+        atoms=watson.log_file_to_atoms("testinputs/problem.md")
         sessions=watson.get_sessions(atoms)
         self.assertEqual(len(sessions),0)
 
     def test_read_desktop_log_file(self):
-        atoms=watson.read_tracking_file("testinputs/desktop.md")
+        atoms=watson.desktop_tracking_file_to_atoms("testinputs/desktop.md")
         self.assertEqual(len(atoms),66)
 
     def test_make_email_sessions(self):
-        atoms=watson.read_tracking_file("testinputs/desktop.md")
+        atoms=watson.desktop_tracking_file_to_atoms("testinputs/desktop.md")
         sessions=watson.get_sessions(atoms)
         self.assertEqual(len(sessions),2)
 
 
 
 
-    def test_read_log_file_blanktitle(self):
-        atoms=watson.read_log_file("testinputs/regressions/livenotes.md")
+    def test_log_file_to_atoms_blanktitle(self):
+        atoms=watson.log_file_to_atoms("testinputs/regressions/livenotes.md")
         self.assertEqual(atoms[0].title,"testinputs/regressions/livenotes.md")
 
-    def test_read_log_file_proper_title(self):
-        atoms=watson.read_log_file("testinputs/regressions/bug-with-markdown-links.md")
+    def test_log_file_to_atoms_proper_title(self):
+        atoms=watson.log_file_to_atoms("testinputs/regressions/bug-with-markdown-links.md")
         self.assertEqual(atoms[0].title,"Bug with markdown links")
 
     def test_make_sessions(self):
-        atoms=watson.read_log_file("testinputs/regressions/livenotes.md")
+        atoms=watson.log_file_to_atoms("testinputs/regressions/livenotes.md")
         sessions=watson.get_sessions(atoms)
         self.assertEqual(len(sessions),36)
 
     def test_read_heartrate_file(self):
-        atoms=watson.read_watch_heartrate("testinputs/heart.csv")
+        atoms=watson.heartrate_to_atoms("testinputs/heart.csv")
         self.assertEqual(len(atoms),164866)
 
     def test_count_awake_sessions(self):
@@ -84,7 +86,7 @@ class watsonTest(TestCase):
         TF = "%d-%b-%Y %H:%M"
         pre=watson.max_dist_between_logs
         watson.max_dist_between_logs=90
-        atoms=watson.read_watch_heartrate("testinputs/heartshort.csv")
+        atoms=watson.heartrate_to_atoms("testinputs/heartshort.csv")
         sessions=watson.get_sessions(atoms)
         watson.max_dist_between_logs=pre
         projects = list(set([entry.project for entry in sessions]))
@@ -96,7 +98,7 @@ class watsonTest(TestCase):
     def test_invert_sessions(self):
         pre=watson.max_dist_between_logs
         watson.max_dist_between_logs=90
-        atoms=watson.read_watch_heartrate("testinputs/heartshort.csv")
+        atoms=watson.heartrate_to_atoms("testinputs/heartshort.csv")
         sessions=watson.get_sessions(atoms)
         #print "XXX{}".format(sessions[0])
         sessions=watson.invert_sessions(sessions)
@@ -109,13 +111,13 @@ class watsonTest(TestCase):
 
     def test_get_exercise_atoms(self):
         TF = "%d-%b-%Y %H:%M"
-        atoms=watson.read_watch_heartrate("testinputs/heartshort.csv")
+        atoms=watson.heartrate_to_atoms("testinputs/heartshort.csv")
         atoms=watson.get_atom_clusters(atoms)
         self.assertEqual(len(atoms),33064)
 
     def test_get_exercise_sessions(self):
         TF = "%d-%b-%Y %H:%M"
-        atoms=watson.read_watch_heartrate("testinputs/heartshort.csv")
+        atoms=watson.heartrate_to_atoms("testinputs/heartshort.csv")
         atoms=watson.get_atom_clusters(atoms)
         sessions=watson.get_sessions(atoms)
         self.assertEqual(len(sessions),58)
@@ -128,7 +130,7 @@ class watsonTest(TestCase):
         setattr(watson.args, 'verbatim',None)
 
         TF = "%d-%b-%Y %H:%M"
-        atoms=watson.read_watch_heartrate("testinputs/heartshort.csv")
+        atoms=watson.heartrate_to_atoms("testinputs/heartshort.csv")
         atoms=watson.get_atom_clusters(atoms)
         sessions=watson.get_sessions(atoms)
         self.assertEqual(len(sessions),58)
@@ -138,10 +140,10 @@ class watsonTest(TestCase):
 
     def test_selective_calendar_write(self):
         TF = "%d-%b-%Y %H:%M"
-        atoms=watson.read_watch_heartrate("testinputs/heartshort.csv")
+        atoms=watson.heartrate_to_atoms("testinputs/heartshort.csv")
         atoms=watson.get_atom_clusters(atoms)
         sessions=watson.get_sessions(atoms)
-        email_atoms=watson.read_tracking_file("testinputs/desktop.md")
+        email_atoms=watson.desktop_tracking_file_to_atoms("testinputs/desktop.md")
         email_sessions=watson.get_sessions(email_atoms)
         sessions.extend(email_sessions)
         watson.calendar_output('testoutputs/exerciseSelective.ics',sessions, 'Exercise')
@@ -167,7 +169,7 @@ class watsonTest(TestCase):
 
     def test_time_split(self):
         TF = "%d-%b-%Y %H:%M"
-        atoms=watson.read_watch_heartrate("testinputs/heartshort.csv")
+        atoms=watson.heartrate_to_atoms("testinputs/heartshort.csv")
         start="02-Jan-2017 12:27"
         end="02-Jan-2017 16:27"
         atoms=watson.cut(atoms,start,end)
--- watson.py
+++ watson.py
@@ -12,108 +12,13 @@ import argparse
 import os
 import json
 import timechart
+from session import Session
+from atom import Atom
 
 __TIME_FORMAT = "%d/%m/%y %H:%M"
 
 max_dist_between_logs = 15  # in minutes TODO these should be arguments for different types of input.
 min_session_size = 15  # in minutes
-
-m = {
-'Jan': 1,
-'Feb': 2,
-'Mar': 3,
-'Apr':4,
- 'May':5,
- 'Jun':6,
- 'Jul':7,
- 'Aug':8,
- 'Sep':9,
- 'Oct':10,
- 'Nov':11,
- 'Dec':12
-}
-
-
-
-
-def fastStrptime(val, format):
-# from http://ze.phyr.us/faster-strptime/
-    l = len(val)
-    if format == '%d/%m/%y %H:%M' and (l == 14):
-        temp= datetime.datetime(
-            2000+int(val[6:8]), # %Y
-            int(val[3:5]), # %m
-            int(val[0:2]), # %d
-            int(val[9:11]), # %H
-            int(val[12:14]), # %M
-            0, # %s
-            0, # %f
-        )
-        return temp
-    # The watch
-    if format == "%d-%b-%Y %H:%M" and (l == 17):
-        temp= datetime.datetime(
-            int(val[7:11]), # %Y
-            m[val[3:6]], # %m
-            int(val[0:2]), # %d
-            int(val[12:14]), # %H
-            int(val[15:17]), # %M
-            0, # %s
-            0, # %f
-        )
-        return temp
-    # Default to the native strptime for other formats.
-    print "falling through {} {} {}".format(val, format, l)
-    return datetime.datetime.strptime(val, format)
-
-class Session(object):
-        project = "Unknown"
-        start = ""
-        end = ""
-        content = ""
-
-        def __init__(self, project, start, end, content):
-                self.project, self.start, self.end = project, start, end
-
-        def length(self):
-                return (self.end-self.start)
-
-        def __str__(self):
-                return "    {} to {} ({})".format(
-                    self.start.strftime("%d/%m/%y %H:%M"), self.end.strftime("%H:%M"), str(self.length())[:-3])
-
-class Atom(object):
-
-        def __init__(self, start="",end="", date="",title="", content="", TF="%d/%m/%y %H:%M"):
-            self.content=start
-            self.start=end
-            self.title=title
-            self.end=end
-            self.date=date
-            self.TF=TF
-            self.s=None
-            self.e=None
-
-        def get_S(self):
-            total_date=self.date+" "+self.start
-            if not self.s:
-                self.s= fastStrptime(total_date,self.TF)
-            return self.s
-
-        def get_E(self):
-            total_date=self.date+" "+self.end
-            if not self.e:
-                self.e= fastStrptime(total_date,self.TF)
-            types=str(type(self.e))
-            if "date" not in types:
-                self.e= fastStrptime(total_date,self.TF)
-#                self.e= datetime.datetime.strptime(total_date,self.TF)
-            #print self.e
-            return self.e
-
-        def __str__(self):
-            return "{}, from {} to {} on {}".format(self.title,self.start,self.end,self.date)
-
 
 def setup_argument_list():
     "creates and parses the argument list for Watson"
@@ -130,6 +35,17 @@ def setup_argument_list():
     return parser.parse_args()
 
 
+# Summary ######################################################################
+
+def output_sessions_as_projects(sessions):
+        total_time = sum([entry.length()
+                          for entry in sessions], datetime.timedelta())
+        projects = list(set([entry.project for entry in sessions]))
+        for project in projects:
+                projectreport(project, sessions, args.verbatim)
+        print "Total project time".ljust(45)+str(total_time)
+        return total_time
+
 def projectreport(name, sessions, verbose):
         project_sessions = [ entry for entry in sessions if ( entry.project == name)]
         total_time = sum([entry.length() for entry in project_sessions], datetime.timedelta())
@@ -142,26 +58,14 @@ def projectreport(name, sessions, verbose):
         return total_time
 
 
-def print_original(atoms):
-    previous_date=""
-    for atom in atoms:
-      if atom['date']==previous_date:
-        print "###### "+atom['start']+ "Where is the end time???"
-      else:
-        print "###### "+atom['date']+ " "+ atom['start']+ "Where is the end time???"
-        previous_date=atom['date']
-      print atom['content']
-      print "____________________________________________________________________________"
+def days_old(session):
+        delta = datetime.datetime.now() - session.start
+	return delta.days
 
-def output_sessions_as_projects(sessions):
-        total_time = sum([entry.length()
-                          for entry in sessions], datetime.timedelta())
-        projects = list(set([entry.project for entry in sessions]))
-        for project in projects:
-                projectreport(project, sessions, args.verbatim)
-        print "Total project time".ljust(45)+str(total_time)
-        return total_time
 
+
+
+########## Processing ##########
 def get_sessions(atoms):
         last= datetime.datetime.strptime( "11/07/10 10:00", __TIME_FORMAT)
         current = atoms[0].get_E()
@@ -178,9 +82,6 @@ def get_sessions(atoms):
 		last = current.get_E()
                 current_group.append(current)
         grouped_timevalues.append(current_group)
-        return filter_by_session_size(grouped_timevalues)
-
-def filter_by_session_size(grouped_timevalues):
         sessions=[]
         for i in grouped_timevalues:
             if i:
@@ -188,7 +89,74 @@ def filter_by_session_size(grouped_timevalues):
                     sessions.append(Session(i[0].title,i[0].get_S(),i[-1].get_E(),i))
         return sessions
 
-def read_log_file(filename, title=None):
+
+def get_atom_clusters(atomsin):
+    atoms=[]
+    lastatom=atomsin[0]
+    for atom in atomsin:
+        if atom.start[:4]== lastatom.start[:4]:
+            atom_minutes=int(atom.start[0:2])*60+int(atom.start[3:5])
+            lastatom_minutes=int(lastatom.start[0:2])*60+int(lastatom.start[3:5])
+            difference=atom_minutes-lastatom_minutes
+            if difference<1:
+                atom.title="Exercise"
+                atoms.append(atom)
+        lastatom=atom
+    return atoms
+
+def make_exercise_file(args,atoms):
+     sessions=get_sessions(get_atom_clusters(atoms))
+     timechart.graph_out(sessions,"exercise")
+     return sessions
+
+def make_sleep_file(args,atoms):
+     global max_dist_between_logs
+     global min_session_size
+     pre=max_dist_between_logs
+     pre2=min_session_size
+     min_session_size = 60  # in minutes
+     max_dist_between_logs=240
+
+     sessions=get_sessions(atoms)
+     sessions=invert_sessions(sessions)
+     max_dist_between_logs=pre
+     min_session_size = pre2
+     return sessions
+
+def make_projects_file(vision_dir, name):
+    atoms=[]
+    for file in glob.glob(vision_dir+"/*.md"):
+        atoms.extend(log_file_to_atoms(file))
+    sessions=get_sessions(atoms)
+    timechart.graph_out(sessions,name)
+    return sessions
+
+
+def cut(atoms,start,end):
+    TF = "%d-%b-%Y %H:%M"
+    start_time= datetime.datetime.strptime( start, TF)
+    end_time= datetime.datetime.strptime( end, TF)
+    return_atoms=[]
+    for current in atoms:
+            if (current.get_S() > start_time):
+                if (current.get_S() < end_time):
+                    return_atoms.append(current)
+    return return_atoms
+
+
+def invert_sessions(sessions):
+    lastsession=sessions[0]
+    new_sessions=[]
+    for session in sessions:
+        new_sessions.append(Session(session.project,lastsession.end,session.start,session.content))
+        lastsession=session
+    return new_sessions
+
+
+########## Input ##########
+
+
+def log_file_to_atoms(filename, title=None):
     if title==None:
 	title=filename
     content=icalhelper.get_content(filename)
@@ -231,7 +199,7 @@ def read_log_file(filename, title=None):
     return atoms
 
 
-def read_watch_heartrate(filename):
+def heartrate_to_atoms(filename):
     #01-May-2017 23:46,01-May-2017 23:46,69.0
     TF = "%d-%b-%Y %H:%M"
     timestamplength=len("01-May-2017 23:46")
@@ -241,8 +209,6 @@ def read_watch_heartrate(filename):
         if args.d:
             index=int(args.d)*1500
             content=content[len(content)-index:]
- #           print "This is {}".format(index)
- #           print "Length of content is {}".format(len(content))
     atoms=[]
     for a in content:
         start=a[datelength+1:timestamplength]
@@ -250,83 +216,12 @@ def read_watch_heartrate(filename):
         end=a[timestamplength+1+datelength+1:(timestamplength*2)+1]
         atoms.append(Atom(start,end,date,"Heartrate","Alive",TF))
     atoms.pop(0)
- #   print "Atoms: {}".format(len(atoms))
     return atoms
 
-
-def get_atom_clusters(atomsin):
-    atoms=[]
-    lastatom=atomsin[0]
-    for atom in atomsin:
-        if atom.start[:4]== lastatom.start[:4]:
-            atom_minutes=int(atom.start[0:2])*60+int(atom.start[3:5])
-            lastatom_minutes=int(lastatom.start[0:2])*60+int(lastatom.start[3:5])
-            difference=atom_minutes-lastatom_minutes
-            if difference<1:
-                atom.title="Exercise"
-                atoms.append(atom)
-        lastatom=atom
-    return atoms
 # From SE
 # http://stackoverflow.com/questions/13728392/moving-average-or-running-mean
 
-
-def make_project_file(filename,name):
-    atoms=[]
-    atoms.extend(read_log_file(filename))
-    sessions=get_sessions(atoms)
-    timechart.graph_out(sessions,name)
-    return sessions
-
-def make_email_file(filename):
-    atoms=read_tracking_file(filename)
-    sessions=get_sessions(atoms)
-    timechart.graph_out(sessions,"email")
-    return sessions
-
-
-def make_exercise_file(args,atoms):
-     atoms=get_atom_clusters(atoms)
-     sessions=get_sessions(atoms)
-     timechart.graph_out(sessions,"exercise")
-     return sessions
-
-def make_sleep_file(args,atoms):
-     global max_dist_between_logs
-     global min_session_size
-     pre=max_dist_between_logs
-     pre2=min_session_size
-     min_session_size = 60  # in minutes
-     max_dist_between_logs=240
-
-     sessions=get_sessions(atoms)
-     sessions=invert_sessions(sessions)
-     max_dist_between_logs=pre
-     min_session_size = pre2
-     return sessions
-
-
-
-def make_journal_files():
-    atoms=[]
-    for file in glob.glob("/home/joereddington/Gromit/*.md"):
-
-        atoms.extend(read_log_file(file))
-    sessions=get_sessions(atoms)
-    timechart.graph_out(sessions,"jurgen")
-    return sessions
-
-
-def make_projects_file(vision_dir):
-    atoms=[]
-    for file in glob.glob(vision_dir+"/*.md"):
-	#print file
-        atoms.extend(read_log_file(file))
-    sessions=get_sessions(atoms)
-    timechart.graph_out(sessions,"projects")
-    return sessions
-
-def read_tracking_file(filename,tag="mail"):
+def desktop_tracking_file_to_atoms(filename,tag="mail"):
     content=icalhelper.get_content(filename)
     matchingcontent= [line for line in content if (tag in line )]
     TF = "%d/%m/%y %H:%M"
@@ -341,32 +236,8 @@ def read_tracking_file(filename,tag="mail"):
     return atoms
 
 
-def days_old(session):
-        delta = datetime.datetime.now() - session.start
-	return delta.days
 
-def invert_sessions(sessions):
-    lastsession=sessions[0]
-    #endsession=sessions[-1]
-    #print endsession
-    #sessions.append(Session(endsession.project,endsession.start+datetime.timedelta(days=2),endsession.end+datetime.timedelta(days=2),endsession.content))
-    new_sessions=[]
-    for session in sessions:
-        new_sessions.append(Session(session.project,lastsession.end,session.start,session.content))
-        lastsession=session
-    return new_sessions
-
-
-def cut(atoms,start,end):
-    TF = "%d-%b-%Y %H:%M"
-    start_time= datetime.datetime.strptime( start, TF)
-    end_time= datetime.datetime.strptime( end, TF)
-    return_atoms=[]
-    for current in atoms:
-            if (current.get_S() > start_time):
-                if (current.get_S() < end_time):
-                    return_atoms.append(current)
-    return return_atoms
+# Output
 
 def calendar_output(filename,sessions, matchString=None):
         cal = icalhelper.get_cal()
@@ -377,6 +248,23 @@ def calendar_output(filename,sessions, matchString=None):
 
 
 
+
+
+def print_original(atoms):
+    previous_date=""
+    for atom in atoms:
+      if atom['date']==previous_date:
+        print "###### "+atom['start']+ "Where is the end time???"
+      else:
+        print "###### "+atom['date']+ " "+ atom['start']+ "Where is the end time???"
+        previous_date=atom['date']
+      print atom['content']
+      print "____________________________________________________________________________"
+
+
+
+# Driver files.
+
 def full_detect(config_file='/config.json'):
     cwd=os.path.dirname(os.path.abspath(__file__))
     config = json.loads(open(cwd+config_file).read())
@@ -386,13 +274,17 @@ def full_detect(config_file='/config.json'):
 	print datetime.datetime.now(pytz.timezone("Europe/London")).strftime("###### "+__TIME_FORMAT)
 	sys.exit()
     sessions=[]
-    pacesetter_sessions=make_project_file(config["pacesetter"],"Pacesetter")
-    delores_sessions=make_project_file(config["delores"],"DELORES")
-    email_sessions=make_email_file(config["desktop"])
-    projects_sessions=make_projects_file(vision_dir)
-    watch_atoms=read_watch_heartrate(config['heart'])
+    pacesetter_sessions=get_sessions(log_file_to_atoms(config["pacesetter"]))
+    email_sessions=get_sessions(desktop_tracking_file_to_atoms(config["desktop"]))
+    watch_atoms=heartrate_to_atoms(config['heart'])
     exercise_sessions=make_exercise_file(args,watch_atoms)
     sleep_sessions=make_sleep_file(args,watch_atoms)
+
+    delores_sessions=get_sessions(log_file_to_atoms(config["delores"]))
+    projects_sessions=make_projects_file(vision_dir, "projects")
+    timechart.graph_out(email_sessions,"email")
+    timechart.graph_out(pacesetter_sessions,"Pacesetter")
+    timechart.graph_out(delores_sessions,"DELORES")
 
     sessions.extend(pacesetter_sessions)
     sessions.extend(delores_sessions)
@@ -400,22 +292,22 @@ def full_detect(config_file='/config.json'):
     sessions.extend(exercise_sessions)
     sessions.extend(projects_sessions)
 
-    calendar_output(cwd+"/calendars/pacesetter.ics",pacesetter_sessions)
-    calendar_output(cwd+"/calendars/email.ics",email_sessions)
-    calendar_output(cwd+"/calendars/projects.ics",projects_sessions)
-    calendar_output(cwd+"/calendars/Exercise.ics",exercise_sessions)
-    calendar_output(cwd+"/calendars/Sleep.ics",sleep_sessions)
     if args.d:
             sessions = [i for i in sessions if days_old(i)<int(args.d)]
             sleep_sessions = [i for i in sleep_sessions if days_old(i)<int(args.d)]
 
 
-
+    time =0
     if args.action == "sleep":
-        return output_sessions_as_projects(sleep_sessions)
+        time= output_sessions_as_projects(sleep_sessions)
     else:
-       return  output_sessions_as_projects(sessions)
+       time=  output_sessions_as_projects(sessions)
 
 
-
+  #  calendar_output(cwd+"/calendars/pacesetter.ics",pacesetter_sessions)
+  #  calendar_output(cwd+"/calendars/email.ics",email_sessions)
+  #  calendar_output(cwd+"/calendars/projects.ics",projects_sessions)
+  #  calendar_output(cwd+"/calendars/Exercise.ics",exercise_sessions)
+  #  calendar_output(cwd+"/calendars/Sleep.ics",sleep_sessions)
+    return time
 
