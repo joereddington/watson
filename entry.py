4a47a55b91dddd4369afa9c335bb82b15b64b226
Joe Reddington <joe@joereddington.com>
2018-08-27 15:28:01 +0100

    first test passing

--- entry.py
+++ entry.py
@@ -1,11 +1,15 @@
-
+import re
+import datetime
 
 class Entry(object):
 
 
         def __init__(self, input_string):
             self.input_string=input_string
-            pass
+            match = re.search(r'\d{2}/\d{2}/\d{2}', input_string)
+            self.date = datetime.datetime.strptime(match.group(), '%y/%m/%d').date()
+            match = re.search(r',\s(?P<title>.*)', input_string)
+            self.title =match.group("title").strip()
 
         def get_title(self):
-            return ""
+            return self.title
