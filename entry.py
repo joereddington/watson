271a4e5a9ee5043137e41cae5e3d525274ac0c90
Joe Reddington <joe@joereddington.com>
2018-08-27 15:30:24 +0100

    Second test passing

--- entry.py
+++ entry.py
@@ -7,7 +7,10 @@ class Entry(object):
         def __init__(self, input_string):
             self.input_string=input_string
             match = re.search(r'\d{2}/\d{2}/\d{2}', input_string)
-            self.date = datetime.datetime.strptime(match.group(), '%y/%m/%d').date()
+            if match:
+                self.date = datetime.datetime.strptime(match.group(), '%y/%m/%d').date()
+            else:
+                self.date = None
             match = re.search(r',\s(?P<title>.*)', input_string)
             self.title =match.group("title").strip()
 
--- test_dr_watson.py
+++ test_dr_watson.py
@@ -21,6 +21,9 @@ class watsonTest(TestCase):
         entry=Entry("###### 27/08/18 00:01 to 07:53, +Sleep")
         self.assertEqual(entry.get_title(),"+Sleep")
 
+    def test_parse_line_without_date(self):
+        entry=Entry("###### 15:17, Making Watson great again.")
+        self.assertEqual(entry.get_title(),"Making Watson great again.")
 
 
 if __name__=="__main__":
